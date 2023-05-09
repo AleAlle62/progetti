97aa38d78cb7175b5b69eeb371781c1110655dc3
AleAlle62 <basketallegrini@gmail.com>
2023-05-09 12:11:43 +0200

    counter

---
+++ prova_vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Counter App</h1>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import CounterComponent from './components/CounterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterComponent
+  }
+}
+</script>
---
+++ prova_vue/src/components/CounterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="increment">+</button>
+        <span>{{ count }}</span>
+        <button @click="decrement">-</button>
+    </div>
+</template>  
+
+<script>
+export default{
+    data() {
+        return {
+        count: 0
+        }
+    },
+    methods: {
+        increment() {
+        this.count++
+        },
+        decrement() {
+        this.count--
+        }
+    }
+}
+</script>
