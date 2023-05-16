12f090b59c29dfa18cd55352e56ff7ddc82ea6f4
AleAlle62 <basketallegrini@gmail.com>
2023-05-16 19:32:36 +0200

    header completo

--- porfolio/portfolio/src/components/HeaderPage.vue
+++ porfolio/portfolio/src/components/HeaderPage.vue
@@ -6,7 +6,7 @@
         <h3>Junior Full Stack Web Developer</h3>
         <img src="../assets/profilo.jpeg" alt="">
       </div>
-      <p>Coding is a never-ending learning journey, where every line of code is an opportunity to grow</p>
+      <p>"Coding is a never-ending learning journey, where every line of code is an opportunity to grow"</p>
     </div>
   </div>
 </template>
@@ -19,15 +19,53 @@ export default {
 </script>
 
 
+<!-- stile -->
 <style scoped>
 .navbar {
   display: flex;
-  justify-content: space-around;
-
+  align-items: center;
+  justify-content: space-between;
 }
 
+/* TITOLO JUNIOR  */
+h3 {
+  background-image: linear-gradient(
+    to right,
+    #54b3d6,
+    #54b3d6 50%,
+    #000 50%
+  );
+  background-size: 200% 100%;
+  background-position: -100%;
+  display: inline-block;
+  padding: 5px 0;
+  position: relative;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  transition: all 0.3s ease-in-out;
+}
+h3:before {
+  content: '';
+  background: #54b3d6;
+  display: block;
+  position: absolute;
+  bottom: -3px;
+  left: 0;
+  width: 0;
+  height: 3px;
+  transition: all 0.3s ease-in-out;
+}
+h3:hover {
+  background-position: 0;
+}
+h3:hover::before {
+  width:100%;
+}
+/* FINE TITOLO */
+
 p {
-  font-size: 80%;
+  font-size: 50%;
+  text-align: center;
 }
 
 img {
